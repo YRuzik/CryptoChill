fc797c3acd78e7ddda2979aab18d98609a74cf84
byehappy <byehappy@yandex.ru>
2023-01-20 15:07:55 +0300

    adaptability About page

--- src/components/aboutInf/About.style.tsx
+++ src/components/aboutInf/About.style.tsx
@@ -3,7 +3,12 @@ import styled, { keyframes } from "styled-components";
 const TextContent =`
     font-family: 'Quicksand';
     font-style: normal;
-    font-size: 30px;
+    @media (min-width: 320px) {
+        font-size:100%;
+    }
+    @media (min-width: 768px) {
+        font-size:150%;
+    }
 `
 
 export const Content = styled.div`
@@ -11,15 +16,26 @@ export const Content = styled.div`
     padding-top: 35px;
     display: grid; 
     grid-auto-columns: 1fr; 
-    grid-template-columns:35% 30%; 
     grid-template-rows: 65%; 
     justify-content:center;
+    @media (min-width: 300px) {
+        grid-template-columns:65%; 
+    }
+    @media (min-width: 1024px) {
+        grid-template-columns:35% 30%; 
+    }
 `
 export const ContentItemMcduck= styled.div`
     position: static;
     transform: scale(125%);
     flex: 0 1 auto;
     padding-top: 1rem;
+    @media (min-width: 300px) {
+        display:none;
+    }
+    @media (min-width: 1024px) {
+        display:block;
+    }
 `
 export const ContentItemText =styled.div`
     position: static;
@@ -27,4 +43,10 @@ export const ContentItemText =styled.div`
     flex-basis:30%;
     text-align:left;
     padding:50px;
+    @media (min-width: 300px) {
+        text-align:center   
+    }
+    @media (min-width: 1024px) {
+        text-align:left   
+    }
 `
